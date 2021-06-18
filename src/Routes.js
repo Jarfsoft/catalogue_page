730c718246074e24173f0d80a5d8c119a7771ae1
Jarfsoft <andresjex@hotmail.com>
2021-06-18 09:36:01 -0600

    Set routes file

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Switch>
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
