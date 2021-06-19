7f286790662e72f0df1bae1ad07079915fdd9eab
Jarfsoft <andresjex@hotmail.com>
2021-06-19 17:14:52 -0600

    Add navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './pokemon.png';
+
+import './Navbar.css';
+
+const Navbar = () => (
+  <main className="navbar">
+    <div className="title">
+      <img src={logo} alt="Logo"/>
+    </div>
+    <div className="links">
+      <p>Type filter: </p>
+      <select>
+        <option>All</option>
+        <option>Fire</option>
+        <option>Water</option>
+        <option>Grass</option>
+        <option>Electric</option>
+        <option>Ice</option>
+        <option>Rock</option>
+        <option>Ground</option>
+        <option>Bug</option>
+        <option>Fight</option>
+        <option>Ghost</option>
+        <option>Fairy</option>
+        <option>Flying</option>
+        <option>Normal</option>
+      </select>
+    </div>
+  </main>
+);
+
+export default Navbar;
--- src/Routes.js
+++ src/Routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import App from './Components/App';
+import Navbar from './Components/Navbar';
 
 const Routes = () => (
   <BrowserRouter>
+    <Navbar />
     <Switch>
       <Route path="/" component={App} />
     </Switch>
